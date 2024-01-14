a5b8dfaace5cd7a991a547e3c5d78809e8d48e84
aajaiiii <125202897+aajaiiii@users.noreply.github.com>
2024-01-14 23:22:01 +0700

    14/01/672321

--- app.js
+++ app.js
@@ -422,6 +422,40 @@ app.delete("/deleteCaremanual/:id", async (req, res) => {
 
 
 //แก้ไขคู่มือ
+// app.post("/updatecaremanual/:id", upload, async (req, res) => {
+//   const { caremanual_name, detail } = req.body;
+//   const { id } = req.params;
+
+//   try {
+//     let imagename = "";
+//     let filename = "";
+
+//     if (req.files['image'] && req.files['image'][0]) {
+//       imagename = req.files['image'][0].filename;
+//     }
+
+//     if (req.files['file'] && req.files['file'][0]) {
+//       filename = req.files['file'][0].filename;
+//     }
+
+//     const updatedCaremanual = await Caremanual.findByIdAndUpdate(id, {
+//       caremanual_name,
+//       image: imagename,
+//       file: filename,
+//       detail,
+//     }, { new: true });
+    
+//     // await Admins.findByIdAndUpdate(id, { password: encryptedNewPassword });
+//     if (!updatedCaremanual) {
+//       return res.status(404).json({ status: "Caremanual not found" });
+//     }
+
+//     res.json({ status: "ok", updatedCaremanual });
+//   } catch (error) {
+//     res.json({ status: error });
+//   }
+// });
+
 app.post("/updatecaremanual/:id", upload, async (req, res) => {
   const { caremanual_name, detail } = req.body;
   const { id } = req.params;
@@ -430,31 +464,60 @@ app.post("/updatecaremanual/:id", upload, async (req, res) => {
     let imagename = "";
     let filename = "";
 
+    // มีรูปใหม่ไหม
     if (req.files['image'] && req.files['image'][0]) {
       imagename = req.files['image'][0].filename;
     }
-
+    // มีไฟล์ไหม
     if (req.files['file'] && req.files['file'][0]) {
       filename = req.files['file'][0].filename;
     }
+    // ตรวจสอบว่ามีการอัปเดตรูปภาพหรือไม่
+    if (imagename !== "") {
+      // มีการอัปเดต
+      const updatedWithImage = await Caremanual.findByIdAndUpdate(id, {
+        caremanual_name,
+        image: imagename,
+        file: filename,
+        detail,
+      }, { new: true });
 
-    const updatedCaremanual = await Caremanual.findByIdAndUpdate(id, {
-      caremanual_name,
-      image: imagename,
-      file: filename,
-      detail,
-    }, { new: true });
-    
-    // await Admins.findByIdAndUpdate(id, { password: encryptedNewPassword });
-    if (!updatedCaremanual) {
-      return res.status(404).json({ status: "Caremanual not found" });
+      if (!updatedWithImage) {
+        return res.status(404).json({ status: "Caremanual not found" });
+      }
+
+      res.json({ status: "ok", updatedCaremanual: updatedWithImage });
+    } else {
+      // ไม่มีการอัปเดต
+      const oleCaremanual = await Caremanual.findById(id);
+
+      if (!oleCaremanual) {
+        return res.status(404).json({ status: "Caremanual not found" });
+      }
+
+      let existingFilename = "";
+      // ตรวจสอบว่ามีการอัปโหลดไฟล์ไม่
+      if (filename !== "") {
+        existingFilename = filename;
+      } else {
+        existingFilename = oleCaremanual.file;
+      }
+
+      // ให้ใช้ค่าเดิมของไฟล์
+      const updatedWithoutFile = await Caremanual.findByIdAndUpdate(id, {
+        caremanual_name,
+        image: oleCaremanual.image,
+        file: existingFilename,
+        detail,
+      }, { new: true });
+0
+      res.json({ status: "ok", updatedCaremanual: updatedWithoutFile });
     }
-
-    res.json({ status: "ok", updatedCaremanual });
   } catch (error) {
     res.json({ status: error });
   }
 });
+
 
 //ดึงคู่มือมา
 app.get('/getcaremanual/:id', async (req, res) => {
